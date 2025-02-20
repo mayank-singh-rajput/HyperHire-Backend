543053afea00b3bbe1aef54d0a6dc25e45265444
Mayank Singh <mayanksingh@Mayanks-MacBook-Pro.local>
2025-02-20 12:09:16 +0530

    vercel build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { FolderModule } from './folder/folder.module';
+import { AuthenticationMiddleware } from './middleware/authentication.check';
 
 @Module({
   imports: [PrismaModule, UserModule, FolderModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes('*');
+  }
+}
